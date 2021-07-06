a9ee4214bab8fff94a4f490002bafc9ddde2c0c8
wikixxyl <raghavkumar0807@gmail.com>
2021-07-06 23:15:02 +0800

    Implemented Virustotal Scanner

--- virusscanner.py
+++ virusscanner.py
@@ -4,7 +4,7 @@ import vt
 def scan():
     global analysis
     # TODO: Store virustotal API Keys in env
-    vt_key = "aafa3531e088d440658570a3059046ac7b4bb304f25f4293e25440959e0d522c"
+    vt_key = "aafa3531e088d440658570a3059046ac7b4bb304f25f4293e25440959e0d522c" #
     fp = ""  # filepath to test virustotal (emptied for commit)
     vt_client = vt.Client(vt_key)  # init virustotal client
     with open(fp, "rb") as f:
