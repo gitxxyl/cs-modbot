e230533cf6fec805a1dc55078a7aad15abbb1fe4
azazazo <65768823+azazazo@users.noreply.github.com>
2021-07-06 20:48:57 +0800

    removed if __name__

--- main.py
+++ main.py
@@ -6,21 +6,18 @@ from discord.ext import commands
 from dotenv import load_dotenv
 from pretty_help import PrettyHelp
 
-if __name__ == '__main__':
-    # get environment variables
-    load_dotenv()
-    TOKEN = os.getenv("DISCORD_TOKEN")
-    PREFIX = os.getenv("PREFIX")
+load_dotenv()
+TOKEN = os.getenv("DISCORD_TOKEN")
+PREFIX = os.getenv("PREFIX")
 
-    # create bot object
-    intents = discord.Intents.default()
-    intents.members = True  # to access members
-    bot = commands.Bot(command_prefix=f"{PREFIX}",
-                       help_command=PrettyHelp(color=discord.colour.Colour.blurple(),
-                                               show_index=False,
-                                               no_category="Commands"),
-                       intents=intents,
-                       case_insensitive=True)
-
-    # add cogs here
-    bot.run(TOKEN)
+# create bot object
+intents = discord.Intents.default()
+intents.members = True  # to access members
+bot = commands.Bot(command_prefix=f"{PREFIX}",
+                   help_command=PrettyHelp(color=discord.colour.Colour.blurple(),
+                                           show_index=False,
+                                           no_category="Commands"),
+                   intents=intents,
+                   case_insensitive=True)
+# add cogs here
+bot.run(TOKEN)
