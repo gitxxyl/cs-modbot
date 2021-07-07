7dc6339952310b0eae42ea6ae20cfa99547f1b20
wikixxyl <raghavkumar0807@gmail.com>
2021-07-07 19:07:10 +0800

    Created config and debug files - config for modifiable global variables and debug for debugging commands e.g. logging.

---
+++ debug.py
@@ -0,0 +1,19 @@
+import datetime
+
+import config
+
+
+async def start(bot):
+    """Initialise debugging features; run at start of script."""
+    global file, channel
+    dt = datetime.datetime.today()
+    channel = await bot.get_channel(config.debug_channel_id)
+    file = open(f"CS-MODBOT Logog-{dt.day}-{dt.month}-{dt.year}.log", "a")  # load log file on import into main
+
+
+async def log(log: any):
+    """Log a message to log file, console and log channel."""
+    global file
+    file.write(str(log))
+    print(log)
+    await channel.send(str(log))
