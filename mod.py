5cdc57a7be71f9c2fd0a54f14231081467fa5503
azazazo <h2010138@nushigh.edu.sg>
2021-07-07 14:43:43 +0800

    Added purge command

--- mod.py
+++ mod.py
@@ -7,6 +7,7 @@ config = dotenv_values(".env")
 TOKEN = config["DISCORD_TOKEN"]
 PREFIX = config["PREFIX"]
 
+
 class Mod(commands.Cog):
     """
     Cog that deals with the main mod commands, like
@@ -16,8 +17,13 @@ class Mod(commands.Cog):
         self.bot = bot
 
     @commands.command(name='purge')
-    async def purge(self, ctx, number: int) -> None:
+    async def purge(self, ctx, amount: int) -> None:
         """
         Deletes messages from the channel that this command is run in.
-        <number>: number of messages to delete
+        <amount>: number of messages to delete
         """
+        if amount <= 0:
+            await ctx.send("You can't delete a negative number of messages!")
+            return
+        await ctx.channel.purge(limit=amount + 1)
+        await ctx.send(f"Purged {amount} messages.", delete_after=2)
